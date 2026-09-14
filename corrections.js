f2c1ffa05e3644adc479cf31691b04ebe2ca8d56
qswa8011908-dot <qswa8011908@gmail.com>
2026-09-14 08:59:45 +0800

    Fix S15 option E to 對錶

--- corrections.js
+++ corrections.js
@@ -1,3 +1,6 @@
-// 題庫校正：複選題 3
+// 題庫校正：複選題 3、單選題 15
 const m3 = BANK.find(q => q.id === 'M3');
 if (m3) m3.choices.E = '對錶';
+
+const s15 = BANK.find(q => q.id === 'S15');
+if (s15) s15.choices.E = '對錶';
